fa1435eedfca3727ebb124990f64a9ab27bd434d
Sislecv <sislecv@github.com>
2026-07-01 17:57:56 +0800

    fix: remove unused WASM bloat, add minimal system.jsdos bundle for DOS boot

--- dos/types/dos/dosbox/ts/direct.d.ts
+++
@@ -1,3 +0,0 @@
-import { WasmModule } from "../../../impl/modules";
-import { TransportLayer, Net } from "../../../protocol/protocol";
-export declare function dosDirect(wasmModule: WasmModule, sessionId: string, canvas?: OffscreenCanvas, audioWorklet?: boolean, net?: Net): Promise<TransportLayer>;
--- dos/types/dos/dosbox/ts/worker.d.ts
+++
@@ -1,3 +0,0 @@
-import { WasmModule } from "../../../impl/modules";
-import { TransportLayer, Net } from "../../../protocol/protocol";
-export declare function dosWorker(workerUrl: string, wasmModule: WasmModule, sessionId: string, canvas?: OffscreenCanvas, audioWorklet?: boolean, net?: Net): Promise<TransportLayer>;
